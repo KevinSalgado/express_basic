3995d7b4df33150388b4f468a5a4273427375116
Kevin_Salgado <sgkevinsg@gmail.com>
2022-04-23 12:19:34 -0500

    final 1

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.get("/launchx", (req, res) => {
   res.send("Bienvenidos a launchx");
 });
 
+app.get("/explorersInNode", (req, res) => {
+  const explorer = { name: "Explorer", msg: "Hello" };
+  res.send(explorer);
+});
+
+app.get("/explorers/:explorerName", (req, res) => {
+  res.send(req.params);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
